d681c232b1545a642c0f8f3aa84f6fc010b6f5ce
wheatgreaser3000 <wheatgreaser3000@gmail.com>
2026-07-29 21:18:10 +0530

    feat: branch init

--- src/graphics/main.cpp
+++ src/graphics/main.cpp
@@ -23,7 +23,7 @@ int main(void)
         const int texture_y = SCREEN_SIZE.y / 2 - texture.height / 2;
         DrawTexture(texture, texture_x, texture_y, WHITE);
 
-        const char* text = "CruX BPHC!";
+        const char* text = "fuck you";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
         DrawText(text, SCREEN_SIZE.x / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
