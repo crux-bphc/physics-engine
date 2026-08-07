b4cd6ffe45c968be663619534a1c9cbaf656c576
wheatgreaser3000 <wheatgreaser3000@gmail.com>
2026-08-07 14:10:31 +0530

    refactor: separate solver cpp file

--- include/eqn-solver/eqn-solver.hpp
+++ include/eqn-solver/eqn-solver.hpp
@@ -5,8 +5,7 @@ namespace solver {
 using Function = std::function<double(double, double)>;
 class Solver {
     public:
-	Solver(double x0, double y0, double x, double h, const Function func):
-	    x0(x0), y0(y0), x(x), h(h), func(func), xn(x0), yn(y0){};
+	Solver(double x0, double y0, double x, double h, const Function func);
 	double x0, y0, x, h, xn, yn;
 	virtual double solve() = 0;
 	const Function func;
---
+++ src/eqn-solver/solver.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <eqn-solver/eqn-solver.hpp>
+
+
+namespace solver {
+    Solver::Solver(double x0, double y0, double x, double h, const Function func) : x0(x0), y0(y0), x(x), h(h), func(func), xn(x0), yn(y0){};
+}
+
